41165f30ee841b4d0a2792b07e5e8cb07b8a176d
Kemosabe2911 <stevinprince29@gmail.com>
2020-10-25 15:25:13 +0530

    Update server

--- server.js
+++ server.js
@@ -2,3 +2,20 @@ const express= require('express');
 const ejs= require('ejs');
 const bodyparser= require('body-parser');
 const nodemailer= require('nodemailer');
+
+//Init Express
+const app= express();
+
+//EJS
+app.set('view engine','ejs');
+
+const PORT= process.env.PORT || 5000;
+
+app.get('/',(req,res) =>{
+    res.send('Hello');
+});
+
+app.listen(PORT, () =>{
+    console.log(`Server Running on port ${PORT}`);
+});
+
